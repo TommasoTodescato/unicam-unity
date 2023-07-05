2ecdd33fecca14cd58311e9b46550be5e5be661d
Tommaso <tommasotodescato@gmail.com>
2023-07-05 11:34:01 +0200

    player movement completely fixed

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -38,9 +38,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
+    private Rigidbody2D body;
 
     void Start()
     {
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -49,6 +51,11 @@ public class PlayerMovement : MonoBehaviour
         move.y = Input.GetAxis("Vertical");
         move = move.normalized;
 
-        transform.Translate(move * speed * Time.deltaTime);
+        // transform.Translate(move * speed * Time.deltaTime);
+
+        float xMove = move.x * speed * Time.deltaTime;
+        float yMove = move.y * speed * Time.deltaTime;
+        Vector2 newPos = new Vector2(body.position.x + xMove, body.position.y + yMove);
+        body.MovePosition(newPos);
     }
 }
