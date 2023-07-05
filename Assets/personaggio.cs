df030069d20046b746ba0fc8433ec4bdfb9798aa
Tommaso <tommasotodescato@gmail.com>
2023-07-05 10:25:02 +0200

    fixed movement

--- Assets/personaggio.cs
+++ Assets/personaggio.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class pallina : MonoBehaviour
 {
     public float speed;
-    private float xInput, yInput;
+    private Vector2 move;
+    bool oldUp;
 
     void Start()
     {
@@ -13,10 +14,35 @@ public class pallina : MonoBehaviour
 
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
-        transform.Translate(translation:Vector3.right * (speed * Time.deltaTime * xInput));
-        yInput = Input.GetAxis("Vertical");
-        transform.Translate(translation: Vector3.up * (speed * Time.deltaTime * yInput));
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+        bool xMoving = move.x != 0;
+        bool yMoving = move.y != 0;
 
+
+        if (xMoving && yMoving)
+        {
+            if (oldUp)
+                move.y = 0;
+            else
+                move.x = 0;
+        }
+        else if (xMoving)
+        {
+            move.y = 0;
+            oldUp = false;
+        }
+        else if (yMoving)
+        {
+            move.x = 0;
+            oldUp = true;
+        }
+        else
+        {
+            move.x = 0;
+            move.y = 0;
+        }
+        
+        transform.Translate(move * speed * Time.deltaTime);
     }
 }
