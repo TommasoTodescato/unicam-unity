43b742370e9f8ff961fee311c82f19bd4b36b6d4
Tommaso <tommasotodescato@gmail.com>
2023-07-05 12:16:23 +0200

    now we can change scenes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 //      TO KEEP FOR ANIMATION   //
@@ -60,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.gameObject.SetActive(false);
-        speed = 30;
+        SceneManager.LoadScene("galaga");
         return;
     }
 }
